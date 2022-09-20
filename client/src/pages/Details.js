ea961bff6f2e6bc8639e750653906efab1d6c1ba
gabrielab22 <gabriela.bilanzic@gmail.com>
2022-09-20 18:18:58 +0200

    "added logic for deadLine"

--- client/src/pages/Details.js
+++ client/src/pages/Details.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import '../App.css';
 import axios from "axios";
+import { isConstructorDeclaration } from "typescript";
+import { NEWDATE } from "mysql/lib/protocol/constants/types";
 
 function Details() {
     const [korisnik, setKorisnik] = useState({});
@@ -9,7 +11,7 @@ function Details() {
 
     useEffect(() => {
         axios.get(`http://localhost:3001/korisnici/byId/${sessionStorage.getItem("user")}`).then((response) => {
-            console.log("korisnik",response.data);
+            console.log("korisnik", response.data);
             setKorisnik(response.data);
             setIznajmljene(response.data.iznajmljene);
         });
@@ -17,26 +19,53 @@ function Details() {
 
     useEffect(() => {
         axios.get("http://localhost:3001/knjige").then((response) => {
-          setListaKnjiga(response.data);
-          console.log("lista",response.data)
+            setListaKnjiga(response.data);
+            console.log("lista", response.data)
         });
     }, []);
 
-    return <div className="App"> 
-        <p>Korisnik: {korisnik.email} </p>
-        {knjige?.map((value,key) => {
-            return (
-                [iznajmljene]?.map((v, k) => {
-                    const uvjet = parseInt(iznajmljene[k + 1]) === knjige[key].id_knjige;
-                    return( uvjet ?
-                            <div>
-                                <p>Naziv iznajmljene knjige: {knjige[key].naziv_knjige} </p>
-                            </div> : null)
+    console.log(knjige?.filter((knjiga) =>
+        iznajmljene.includes(parseInt(knjiga.id_knjige)) ? knjiga?.naziv_knjige : null
+    ).filter(Boolean).map(({ naziv_knjige, updatedAt }) => {
+        return ({ naziv_knjige, datum: new Date(new Date(updatedAt).getTime() + (1000 * 60 * 60 * 24 * 20)) }
+        )
+    }));
 
-                    })     
+    const zakasnina = (updatedAt) => {
+        const current = new Date().getTime();
+        const deadLine = new Date(updatedAt).getTime() + (1000 * 60 * 60 * 24 * 20);
+        if (current > deadLine) {
+            return (
+                <>
+                    Zakasnina : 50 kuna
+                </>
+            )
+        }
+        else {
+            return (
+                <>
+                    Zakasnina : 0 kuna
+                </>
+            )
+        }
+    }
+    return <div className="App">
+        <p> <b>Korisnik:</b> {korisnik.email} </p>
+        <p> <b>Password:</b> ******</p>
+        {knjige?.filter((knjiga) =>
+            iznajmljene.includes(parseInt(knjiga.id_knjige)) ? knjiga?.naziv_knjige : null
+        ).filter(Boolean).map(({ naziv_knjige, updatedAt }) => {
+            return (
+                <>
+                    <p><b>Naziv iznajmljene knjige:</b>{naziv_knjige}</p>
+                    <p><b>Knjigu treba vratiti datuma: </b>{new Date(new Date(updatedAt).getTime() + (1000 * 60 * 60 * 24 * 20)).toString()}</p>
+                    {zakasnina(updatedAt)}
+                </>
             )
         })}
-         </div>;  
+
+
+    </div>;
 }
 
 export default Details;
